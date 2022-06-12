57f1f69f7ae4b712d5726ce2b55ba7a76591baa8
SashaMaslak <owmaslak@gmail.com>
2022-06-12 10:50:59 +0300

    start

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+// Change code below this line
+
+import { galleryItems } from './gallery-items.js';
+
+const galeryContainer = document.querySelector('.gallery');
+const imagesMarkUp = createGalleryMarkup(galleryItems);
+galeryContainer.insertAdjacentHTML('beforeend', imagesMarkUp);
+
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250'});
+
+function createGalleryMarkup(galleryItems) {
+   return galleryItems
+      .map(({ preview, original, description }) => {
+      return `
+         <a class="gallery__item" href="${original}">
+            <img 
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" />
+         </a>
+      `;
+      })
+      .join('');
+};
